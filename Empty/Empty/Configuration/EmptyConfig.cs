33f3fa9531b51857967be2bdc15ee47b9a1b5757
Mehmet Murat AY(BilgeAdam Ogrenci) <mehmet.ay@baogrenci.com>
2019-04-02 20:21:47 +0300

    empty dosyasına ekleme yaptım

--- Empty/Empty/Configuration/EmptyConfig.cs
+++ Empty/Empty/Configuration/EmptyConfig.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 
 namespace Empty.Configuration
 {
-    public class EmptyConfig
+    public static class EmptyConfig
     {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
     }
 }
